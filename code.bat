35afb1a724747ac0dde3f4da988d3a765261d32b
Kakkann <kaneki.adictter@gmail.com>
2024-11-22 22:44:37 -0300

    create and edit tasks, code refactor

--- code.bat
+++
@@ -1,3 +0,0 @@
-cd d:\Users\User\Documents\Programacao\Laravel\task-list
-start "" code .
-exit
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
+use \App\Models\Task;
 use App\Models\User;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 $json = file_get_contents('users.json', true);
 $users = json_decode($json, true);
@@ -13,13 +16,40 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()->get(),
+        'tasks' => Task::latest()->get(),
     ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) {
-    return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
+
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', ['task' => $task]);
+})->name('tasks.edit');
+
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show', ['task' => $task]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (TaskRequest $request) {
+    $task = Task::create($request->validated());
+
+    return redirect()->route('tasks.show', ['task' => $task])
+        ->with('success', 'Task created successfully!');
+})->name('tasks.store');
+
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    $task->update($request->validated());
+
+    return redirect()->route('tasks.show', ['task' => $task])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
+
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+    return redirect()->route('tasks.index')->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
+
 
 // Route::get('/hello', function () {
 //     return 'Hello World';
